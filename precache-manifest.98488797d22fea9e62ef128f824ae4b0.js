0fc29ab44d8ee6c8d0391682c5f38632869db578
samgsquared <sam@grossmangerlach.com>
2023-04-19 22:51:57 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.98488797d22fea9e62ef128f824ae4b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7a438a7541e1d37d10b",
+    "url": "/static/css/main.79c6b7f6.chunk.css"
+  },
+  {
+    "revision": "b7a438a7541e1d37d10b",
+    "url": "/static/js/main.b7a438a7.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ef1b3a9cda004457683c04c1dd860dad",
+    "url": "/index.html"
+  }
+];
